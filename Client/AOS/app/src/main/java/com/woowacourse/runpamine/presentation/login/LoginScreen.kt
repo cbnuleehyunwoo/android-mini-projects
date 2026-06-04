c1d8a8f17216f751bed5b9cf3b33394df27f0f74
cksckckcks <chan679999@naver.com>
2026-06-04 10:52:50 +0900

    feat: 로그인 스크린 구현

---
+++ Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/login/LoginScreen.kt
@@ -0,0 +1,46 @@
+package com.woowacourse.runpamine.presentation.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.woowacourse.runpamine.R
+import com.woowacourse.runpamine.presentation.component.RunpamineLogo
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding((42.5).dp),
+            verticalArrangement = Arrangement.spacedBy(40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            RunpamineLogo()
+
+            Image(
+                painter = painterResource(R.drawable.btn_kakao_login),
+                contentDescription = "카카오 로그인 버튼",
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen()
+}
