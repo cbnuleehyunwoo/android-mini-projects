b8d298e2b027d5c502b12d219a80e5b5c4cdea11
joon0447 <joon0447@naver.com>
2026-06-08 15:35:33 +0900

    refactor: 팀 스크린 컴포넌트 수정

--- Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/team/components/team/RunnerThumbnail.kt
+++ Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/team/components/team/RunnerThumbnail.kt
@@ -1,14 +1,12 @@
 package com.woowacourse.runpamine.presentation.team.components.team
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,8 +19,7 @@ fun RunnerThumbnail(modifier: Modifier = Modifier) {
     Box(
         modifier =
             modifier
-                .aspectRatio(1f)
-                .background(Color.Black),
+                .aspectRatio(1f),
         contentAlignment = Alignment.Center,
     ) {
         Image(
--- Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/team/components/team/TeamSummaryCard.kt
+++ Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/team/components/team/TeamSummaryCard.kt
@@ -1,6 +1,6 @@
 package com.woowacourse.runpamine.presentation.team.components.team
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,14 +24,20 @@ fun TeamSummaryCard(
     label: String,
     modifier: Modifier = Modifier,
 ) {
+    val cardShape = RoundedCornerShape(14.dp)
+
     Column(
         modifier =
             modifier
                 .height(72.dp)
-                .border(
-                    width = 1.2.dp,
-                    color = Blue40,
-                    shape = RoundedCornerShape(14.dp),
+                .shadow(
+                    elevation = 8.dp,
+                    shape = cardShape,
+                    ambientColor = Color.Transparent,
+                    spotColor = Color.Black.copy(alpha = 0.8f),
+                ).background(
+                    color = Color.White,
+                    shape = cardShape,
                 ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
