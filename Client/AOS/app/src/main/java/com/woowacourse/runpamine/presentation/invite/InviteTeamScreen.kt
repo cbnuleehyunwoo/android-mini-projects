e75be1052cfd1117f477082993847e243127f018
Sangjun Jo <94978051+joon0447@users.noreply.github.com>
2026-06-09 16:26:52 +0900

    style: 안드로이드 팀 초대 화면 수평 패딩 추가 (#130)

--- Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/invite/InviteTeamScreen.kt
+++ Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/invite/InviteTeamScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,7 +29,8 @@ fun InviteTeamScreen(
     val clipboardManager = LocalClipboardManager.current
 
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .padding(horizontal = 24.dp),
     ) {
         ScreenTopBar(
             title = stringResource(R.string.invite_team_bar),
