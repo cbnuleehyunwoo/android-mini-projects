f8b896204b4c2ea84deb3d9d4bc2b8525122a62a
joon0447 <joon0447@naver.com>
2026-06-11 16:06:26 +0900

    refactor: 바텀 탭 네비게이션 로직 수정 및 statusBarsPadding 제거
    - 바텀 탭 이동 시 기존 탭의 백스택을 제거하도록 네비게이션 로직 변경
    - 여러 화면에서 미사용되는 statusBarsPadding 제거

--- Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/mypage/MyPageScreen.kt
+++ Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/mypage/MyPageScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
