d765a07b1e2d73925a49e62e168a36f1209f1fb1
cksckckcks <chan679999@naver.com>
2026-06-15 17:01:04 +0900

    feat: 안드로이드 온보딩 스크린 구현 및 Nav 연결

--- Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/navigation/NavHost.kt
+++ Client/AOS/app/src/main/java/com/woowacourse/runpamine/presentation/navigation/NavHost.kt
@@ -16,6 +16,7 @@ import com.woowacourse.runpamine.presentation.invite.InviteTeamScreen
 import com.woowacourse.runpamine.presentation.join.JoinScreen
 import com.woowacourse.runpamine.presentation.login.LoginScreen
 import com.woowacourse.runpamine.presentation.login.viewmodel.LoginDestination
+import com.woowacourse.runpamine.presentation.onboarding.OnboardingScreen
 import com.woowacourse.runpamine.presentation.mypage.MyPageScreen
 import com.woowacourse.runpamine.presentation.nickname.ChangeNicknameScreen
 import com.woowacourse.runpamine.presentation.ranking.RankingScreen
@@ -52,6 +53,18 @@ fun NavHost(
                         }
                     navController.navigate(route) {
                         popUpTo(AppRoute.Splash.route) {
+                            inclusive = true
+                        }
+                    }
+                },
+            )
+        }
+
+        composable(AppRoute.Onboarding.route) {
+            OnboardingScreen(
+                onStartClick = {
+                    navController.navigate(AppRoute.Home.route) {
+                        popUpTo(navController.graph.id) {
                             inclusive = true
                         }
                     }
@@ -229,7 +242,7 @@ fun NavHost(
                     }
                 },
                 onJoinClick = {
-                    navController.navigate(AppRoute.ChangeNickname.route) {
+                    navController.navigate(AppRoute.SetupNickname.route) {
                         popUpTo(AppRoute.TermsAgreement.route) {
                             inclusive = true
                         }
@@ -244,6 +257,19 @@ fun NavHost(
                 onCompleted = {
                     navController.navigate(AppRoute.Home.route) {
                         popUpTo(AppRoute.ChangeNickname.route) {
+                            inclusive = true
+                        }
+                    }
+                },
+            )
+        }
+
+        composable(AppRoute.SetupNickname.route) {
+            ChangeNicknameScreen(
+                onBackClick = navController::popBackStack,
+                onCompleted = {
+                    navController.navigate(AppRoute.Onboarding.route) {
+                        popUpTo(AppRoute.SetupNickname.route) {
                             inclusive = true
                         }
                     }
