cdf1dbe0b6211873b3afc9f78f5a777f7b5abc3f
Sangjun Jo <94978051+joon0447@users.noreply.github.com>
2026-06-17 17:56:07 +0900

    chore: 앱 버전 정보 업데이트 (#418)

--- Client/AOS/app/build.gradle.kts
+++ Client/AOS/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.woowacourse.runpamine"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_URL", runpamineProperty("base_url").toBuildConfigString())
